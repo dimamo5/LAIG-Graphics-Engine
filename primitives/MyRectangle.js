6b6546df656d38c8948ab427039f4ad30ad21f53
Diogo Moura <diogomoura13@gmail.com>
2015-09-29 15:37:50 +0100

    Primitives Created

---
+++ primitives/MyRectangle.js
@@ -0,0 +1,41 @@
+
+function MyRectangle(scene, x1,y1,x2,y2){
+    CGFobject.call(this,scene);
+
+    this.x1 = x1;
+    this.y1 = y1;
+    
+    this.x2 = x2;
+    this.y2 = y2;
+
+    this.initBuffers();
+}
+
+MyRectangle.prototype = Object.create(CGFobject.prototype);
+MyRectangle.prototype.constructor = MyRectangle;
+
+MyRectangle.prototype.initBuffers = function() {
+
+    this.vertices = [
+        this.x1,this.y1,0,
+        this.x1,this.y2,0,
+        this.x2,this.y1,0,
+        this.x2,this.y2,0
+    ];
+
+    this.indices = [
+        3,1,0,
+        0,2,3
+    ];
+
+
+	this.normals = [
+			0,0,1,
+			0,0,1,
+			0,0,1,
+			0,0,1
+    ]
+
+    this.primitiveType=this.scene.gl.TRIANGLES;
+	this.initGLBuffers();
+}
